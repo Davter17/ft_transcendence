1e55aaec51b01735eba60447acb533bc963c9338
Alvaro297 <alvaromartinfalagan29@gmail.com>
2025-12-01 16:01:45 +0100

    Plugins actualizados y regex hecho

--- backend_python/api/users/create_user.py
+++ backend_python/api/users/create_user.py
@@ -1,6 +1,14 @@
 from ..conexion.conexion import get_connection
 import bcrypt
+import re
 
+
+def isValidUser(user):
+	regex = r'^[a-zA-Z0-9.%-]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,}$'
+
+	if re.match(user, regex):
+		return True
+	return False
 
 def create_user(json_data: dict):
 
@@ -14,22 +22,23 @@ def create_user(json_data: dict):
 		}
 	
 	if "User" in json_data and "Password" in json_data:
-		cursor = conexion.cursor(dictionary=True)
-		password_bytes = json_data['Password'].encode('utf-8')
-		salt = bcrypt.gensalt()
-		hashed_password = bcrypt.hashpw(password_bytes, salt)
-		query = "INSERT INTO users (username, password) VALUES (%s, %s)"
-		cursor.execute(query, (json_data['User'], hashed_password.decode('utf-8')))
-		conexion.commit()
-		conexion.close()
-		
-		return {
-			"success": True,
-			"message": "Usuario creado exitosamente",
-			"username": json_data['User']
-		}
+		if isValidUser(json_data["User"]):
+			cursor = conexion.cursor(dictionary=True)
+			password_bytes = json_data['Password'].encode('utf-8')
+			salt = bcrypt.gensalt()
+			hashed_password = bcrypt.hashpw(password_bytes, salt)
+			query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+			cursor.execute(query, (json_data['User'], hashed_password.decode('utf-8')))
+			conexion.commit()
+			conexion.close()
+
+			return {
+				"success": True,
+				"message": "Usuario creado exitosamente",
+				"username": json_data['User']
+			}
 
 	return {
 		"success": False,
-		"message": "Función createUser no implementada aún"
+		"message": "Usuario o contraseña incorrecta"
 	}
